da645cb1f2041b2ca09c2d6b3f7300fd76433bad
Lin Kon <you@example.com>
2023-01-24 07:44:00 +0600

    added m sort

--- C3 Working with C plus plus/8-4 Merge Sort Implementation.cpp
+++ C3 Working with C plus plus/8-4 Merge Sort Implementation.cpp
@@ -3,40 +3,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-vector<int>merge_sort(vector<int>a)
+vector<int> merge_sort(vector<int> a)
 {
-    //base case
-    if(a.size() <= 1)
+    // base case
+    if (a.size() <= 1)
     {
         return a;
     }
 
     int mid = a.size() / 2;
-    vector<int>b;
-    vector<int>c;
+    vector<int> b;
+    vector<int> c;
 
-
-    for(int i =0; i<mid; i++)
+    for (int i = 0; i < mid; i++)
         b.push_back(a[i]);
 
-    for(int i =mid; i<a.size(); i++)
-        b.push_back(a[i]);
+    for (int i = mid; i < a.size(); i++)
+        c.push_back(a[i]);
 
+    vector<int> sorted_b = merge_sort(b);
+    vector<int> sorted_c = merge_sort(c);
 
-    vector<int>sorted_b = merge_sort(b);
-    vector<int>sorted_c = merge_sort(c);
+    vector<int> sorted_a;
+    int idx1 = 0;
+    int idx2 = 0;
 
-//    if(a.size() == 1)
-//    {
-//        return a;
-//    }
-
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (idx1 == sorted_b.size())
+        {
+            sorted_a.push_back(sorted_c[idx2]);
+            idx2++;
+        }
+        else if (idx2 == sorted_c.size())
+        {
+            sorted_a.push_back(sorted_b[idx1]);
+            idx1++;
+        }
+        else if (sorted_b[idx1] < sorted_c[idx2])
+        {
+            sorted_a.push_back(sorted_b[idx1]);
+            idx1++;
+        }
+        else
+        {
+            sorted_a.push_back(sorted_c[idx2]);
+            idx2++;
+        }
+    }
+    return sorted_a;
+    //cout<<"hello mid"<<endl;
+    //    if(a.size() == 1)
+    //    {
+    //        return a;
+    //    }
 }
 
 int main()
 {
-
-
+//    cout<<"Hello CPP ;"<<endl;
+    vector<int> a = {9,8,7,4,5,1};
+    vector<int> ans = merge_sort(a);
+    for(int i = 0; ans.size(); i++)
+    {
+        cout<<ans[i]<<" ";
+    }
     return 0;
 }
